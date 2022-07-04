a37eeb037d82848f80e091cf612347d02a69c090
NebulousDev <nebulousdev@gmail.com>
2022-07-04 10:38:43 -0400

    Reverted Math.h to Include folder (to resolve math.h conflict)

--- Include/Math.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Math/Util.h"
-#include "Math/Vector.h"
-#include "Math/Matrix.h"
-#include "Math/Quaternion.h"
---
+++ Include/Math/Math.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Util.h"
+#include "Vector.h"
+#include "Matrix.h"
+#include "Quaternion.h"
